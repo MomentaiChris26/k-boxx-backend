dcb370e378756413cb87866fe6870892398c75b6
Chris Tri <christophertri90@gmail.com>
2020-01-19 02:55:34 +1100

    added mongoose

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config()
+const mongoose = require("mongoose");
+const cors = require('cors');
 
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5500;
 
+const dbConfig = {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
 
-const dbConfig = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true };
-
-mongoose.connect(process.env.DB_URL_TEST, dbConfig, (err) => {
-  if (err)
-      console.error("Error ❌");
-  else
-      console.log("Connected to db ✅");
-
+mongoose.connect(process.env.DB_URL_TEST, dbConfig, err => {
+  if (err) console.error("Error ❌");
+  else console.log("Connected to db ✅");
 });
+
 
 app.use(express.json());
 app.use(cors());
 
+app.use(require('./routes/index'))
 
 
-app.listen(PORT,
-  () => console.log(`Listening on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
