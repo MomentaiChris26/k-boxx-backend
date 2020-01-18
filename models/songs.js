bc792ed7d933c2fcce6045ec05af4a6474b0d5ee
Chris Tri <christophertri90@gmail.com>
2020-01-19 03:04:27 +1100

    added schema and song route

---
+++ models/songs.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+require("dotenv").config();
+
+const songSchema = new Schema(
+  {
+    song_name: {
+      type: String,
+      required: true
+    },
+    artist: {
+      type: String,
+      required: true
+    },
+    language: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      require: true
+    }
+  },
+  {
+    collection: "events"
+  }
+);
+
+const Songs = mongoose.model("Song", songSchema);
+
+module.exports = Songs;
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+router.use('/songs',require('./songs_route'));
+
+module.exports = router;
